7679d0ef684434571282cee37e59d2bd041ba226
DanielContador <danielcontadorb@gmail.com>
2022-05-10 19:13:22 -0400

    Modelado BD Django

--- shiluc/horas/models.py
+++ shiluc/horas/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Cliente(models.Model):
     auto_increment_id = models.AutoField(primary_key=True)
-    nombreCliente = models.CharField(max_length=50,verbose_name='Cliente', blank=False, null=False)
+    nombreCliente = models.CharField(max_length=50,verbose_name='Nombre cliente', blank=False, null=False)
     nombreUsuario = models.CharField(max_length=16,verbose_name='Nombre Usuario', blank=False, null=False)
-    correo = models.CharField(max_length=50,verbose_name='correo', blank=False, null=False)
-    telefono = models.IntegerField(verbose_name='telefono', blank=False, null=False)
-    claveUsuario = models.CharField(max_length=12,verbose_name='claveUsuario', blank=False, null=False)
+    correo = models.CharField(max_length=50,verbose_name='Correo', blank=False, null=False)
+    telefono = models.IntegerField(verbose_name='teléfono', blank=False, null=False)
+    claveUsuario = models.CharField(max_length=12,verbose_name='Contraseña', blank=False, null=False)
     
 
 
@@ -17,7 +17,7 @@ class Cliente(models.Model):
 
 class Servicio(models.Model):
     auto_increment_id = models.AutoField(primary_key=True)
-    tipoServicio = models.CharField(max_length=20, verbose_name='tipoServicio', blank=False, null=False)
+    tipoServicio = models.CharField(max_length=20, verbose_name='Tipo de servicio', blank=False, null=False)
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
 
     def __str__(self):
