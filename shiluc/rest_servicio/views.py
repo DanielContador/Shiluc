106385fc066f8b4dc89a382169a753e8ee79ed1e
DanielContador <danielcontadorb@gmail.com>
2022-06-28 15:48:55 -0400

    api, token, oauth

--- shiluc/rest_servicio/views.py
+++ shiluc/rest_servicio/views.py
@@ -12,34 +12,50 @@ from horas.forms import ServicioForm
 
 # Create your views here.
 
+@api_view(['GET','PUT','DELETE'])
+@permission_classes((IsAuthenticated,))
+
+def detalle_servicio(request, id):
+    try:
+        servicio = Servicio.objects.get(id=id)
+    except Servicio.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = ServicioSerializer(servicio)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        dataP = JSONParser().parse(request)
+        serializer = ServicioSerializer(servicio, data=dataP)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        servicio.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+        
+
 @csrf_exempt
 @api_view(['GET','POST'])
-#@permission_classes((IsAuthenticated,))
+@permission_classes((IsAuthenticated,))
 def lista_servicio(request):
-    datos = {
-        'form':ServicioForm()
-    }
     if request.method =='GET':
         listaServicio = Servicio.objects.all()
         serializer = ServicioSerializer(listaServicio, many = True)
-        return render(request, 'horas/index.html', listaServicio)
+        return Response(serializer.data)
     elif request.method == 'POST':
-                #esto debe rescatarlo del form
-        dataP = ServicioForm(request.POST)
+        dataP = JSONParser().parse(request)
         serializer = ServicioSerializer(data=dataP)
         if serializer.is_valid():
             serializer.save()
-            datos['mensajeAPI'] = 'Guardado'
-            return render(request, 'horas/agregarservicio.html', datos)
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-        
-        
 
 
-
-
-
+"""
 @api_view(['GET','PUT','DELETE'])
 @permission_classes((IsAuthenticated,))
 def detalle_servicio(request, id):
@@ -63,3 +79,4 @@ def detalle_servicio(request, id):
         servicio.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
 
+"""
